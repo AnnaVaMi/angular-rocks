aa0d296b6ff068fa8f6cfb5c91f731190a8506dc
AnnaVaMi <72731876+AnnaVaMi@users.noreply.github.com>
2021-01-20 18:44:47 +0100

    Add styles and tags
    Arreglo de estilos y añadido tags dinámicos

--- src/app/bands/band-detail/band-detail.component.ts
+++ src/app/bands/band-detail/band-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, Meta } from '@angular/platform-browser';
 import { Subscription } from 'rxjs';
 
 import BandList from 'src/assets/json/bandas.json';
@@ -17,7 +17,7 @@ export class BandDetailComponent implements OnInit {
   id: number;                      // id de la banda a mostrar
   private _routeSub: Subscription; // informacion de la url para obtener el id de la banda
   
-  constructor(private _sanitizer: DomSanitizer, private _route: ActivatedRoute) { 
+  constructor(private _sanitizer: DomSanitizer, private _route: ActivatedRoute, private meta:Meta) { 
     
   }
 
@@ -41,6 +41,10 @@ export class BandDetailComponent implements OnInit {
       let bandDetail = this.bandList.find(item => item.id == this.id);
       this.bandDetail = new Band(bandDetail.id, bandDetail.name, bandDetail.description, bandDetail.imagePath, bandDetail.videoPath);
     });
+
+    // Tags dinamicos
+    this.meta.updateTag({name: 'keywords', content: 'banda, rock, ' + this.bandDetail.name.split(' ').toString()});
+    this.meta.updateTag({name: 'description', content: 'Banda de rock ' + this.bandDetail.name});
   }
 
   ngOnDestroy() { this._routeSub.unsubscribe(); }
