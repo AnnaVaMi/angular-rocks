afc76ffd1f38743831defcb83c2c78490271ef45
AnnaVaMi <72731876+AnnaVaMi@users.noreply.github.com>
2021-01-20 14:21:43 +0100

    Add new band to list
    Funcionalidad para añadir bandas nuevas al listado

--- src/app/bands/band-list/band-list.component.ts
+++ src/app/bands/band-list/band-list.component.ts
@@ -10,14 +10,27 @@ import BandList from 'src/assets/json/bandas.json';
 })
 export class BandListComponent implements OnInit {
   bandList: any = BandList;
-  bandsId: number[] = [1,2,3];
+  bandsId: number[] = [2,6];
   actualBands: Band[]=[];
+  newBand: string;
 
   constructor() {
     for(let id of this.bandsId) {
       let band = this.bandList.find(item => item.id == id);
       let new_band = new Band(band.id, band.name, band.description, band.imagePath, band.videoPath);
       this.actualBands.push(new_band);
+    }
+  }
+
+  addNewBand() {
+    let name = this.newBand
+    let band = this.bandList.find(item => item.name.toLowerCase() == name.toLowerCase());
+    if(band) {
+      let new_band = new Band(band.id, band.name, band.description, band.imagePath, band.videoPath);
+      this.actualBands.find(item => item.id == band.id) ? alert('¡Ya tienes esta banda en tu lista!') : this.actualBands.push(new_band);
+    }
+    else {
+      alert('¡Lo siento, banda no encontrada!');
     }
   }
 
